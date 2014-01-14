0d878ff077776e715f6d3f11e25ab653619570e0
Andreas Rohner <andreas.rohner@gmx.net>
2014-01-14 19:12:17 +0100

    log-structured file systems use a garbage collector to free up space. If the device is nearly full a call to open may fail. This patch adds a loop that tries to open the file three times and sleeps every time for 10 seconds to give the garbage collector time to free up space.

--- operations.cpp
+++ operations.cpp
@@ -347,7 +347,14 @@ void writeFile(unordered_multimap<NFS_ID, NFSTree *> &fhmap, const NFSFrame &req
 			mode |= O_TRUNC;
 		}
 
-		int fd = open(path.c_str(), mode, S_IRUSR | S_IWUSR);
+		int i, fd = -1;
+		//try three times to open the file and then give up
+		for (i = 0; i < 3; ++i) {
+			if ((fd = open(path.c_str(), mode, S_IRUSR | S_IWUSR)) != -1)
+				break;
+			sleep(10);
+		}
+
 		if (fd == -1){
 			wperror("ERROR writing to file");
 		} else {
